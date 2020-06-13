b92cf155d6464e74998bc3944ca641cd9366a574
aaronmatson1 <matsonaaron1@gmail.com>
2020-06-12 20:24:59 -0600

    Created and connected API

--- api/server.js
+++ api/server.js
@@ -11,8 +11,23 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(logger);
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.use('/api/auth', authRouter);
+server.use('/api/jokes', authenticate, jokesRouter);
+
+server.unsubscribe('/', (req, res) => {
+    res.status(200).send('<h1>Welcome to the API<h1>');
+});
+
+function logger(req, res, next) {
+    console.log(
+        `${new Date().toISOString()} ${req.ip} ${req.method} ${req.url}`
+    )
+    next();
+}
 
 module.exports = server;
